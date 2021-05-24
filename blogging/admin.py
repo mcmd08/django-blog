9af6c3fec508681ade554df7b166d01badaff30f
mcmd08 <mmdacutanan@gmail.com>
2021-05-23 20:52:00 -0700

    Commit with Blogging App and categories

---
+++ blogging/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from blogging.models import Post, Category
+
+# Register your models here.
+
+
+
+class CategoryInLine(admin.TabularInline):
+    model = Category.posts.through
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInLine,
+        ]
+
+class CategoryAdmin(admin.ModelAdmin):
+    # inlines = [
+    #     CategoryInLine,
+    #     ]
+    exclude = ('posts',)
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
---
+++ blogging/tests.py
@@ -0,0 +1,78 @@
+import datetime
+from django.utils.timezone import utc
+from django.test import TestCase
+from blogging.models import Post, Category
+from django.contrib.auth.models import User
+
+
+# Create your tests here.
+class PostTestCase(TestCase):
+    def test_string_representation(self):
+        expected = 'This is a title'
+        p1 = Post(title=expected)
+        actual = str(p1)
+        self.assertEqual(expected, actual)
+
+
+class CategoryTestCase(TestCase):
+    def test_string_representation(self):
+        expected = 'A Category'
+        c1 = Category(name=expected)
+        actual = str(c1)
+        self.assertEqual(expected, actual)
+
+class FrontEndTestCase(TestCase):
+    '''test views provided in the front-end'''
+
+    fixtures = ['blogging_test_fixture.json',]
+
+    def setUp(self):
+        self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
+        self.timedelta = datetime.timedelta(15)
+        # print('now is', self.now)
+        # print('time delta is', self.timedelta)
+        author = User.objects.get(pk=1)
+        for count in range(1, 11):
+            post = Post(title='Post %d Title' % count,
+                        text = 'foo',
+                        author = author
+                        )
+
+            if count < 6:
+                # publish the first 5  posts
+                pubdate = self.now - self.timedelta * count
+                post.published_date =  pubdate
+                post.save()
+        
+    def test_list_only_published(self):
+        resp = self.client.get('/')
+        # print('resp type is', resp)
+        # the content of the rendered response is always a bytestring
+        resp_text = resp.content.decode(resp.charset)
+        # print('resp text is', resp_text)
+        self.assertTrue('Recent Posts' in resp_text)
+        
+        for count in range(1, 11):
+            title = 'Post %d Title' % count
+
+        if count < 6:
+            self.assertContains(resp, title, count=1)
+        else:
+            self.assertNotContains(resp, title)
+    
+    def test_details_only_published(self):
+        for count in range(1, 11):
+            title = 'Post %d Title' % count
+            print('title is', title)
+            post = Post.objects.get(title=title)
+            print('post is', post, post.pk)
+            resp = self.client.get('/posts/%d/' % post.pk)
+            print('resp is', resp)
+            
+            if count < 6:
+                self.assertEqual(resp.status_code, 200)
+                self.assertContains(resp, title)
+            else:
+                self.assertEqual(resp.status_code, 404)
+            
+            
