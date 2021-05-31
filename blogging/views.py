b1e7d605d9a2acc172caff912895d2a7ae91e0ec
mcmd08 <mmdacutanan@gmail.com>
2021-05-30 21:40:14 -0700

    Initial Commit for Assignment08

--- blogging/views.py
+++ blogging/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 def stub_view(request, *args, **kwargs):
@@ -16,28 +18,40 @@ def stub_view(request, *args, **kwargs):
 
     return HttpResponse(body, content_type='text\plain')
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    # template = loader.get_template('blogging/list.html')
-    # print('posts dct is', posts)
-    # print('template is', template)
-    context = {'posts': posts}
-    # print('context is', context)
-    # body = template.render(context)
-    # print('body is', body)
-    # return HttpResponse(body, content_type='text/html')
-    return render(request, 'blogging/list.html', context)
+# Function-based views
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     # template = loader.get_template('blogging/list.html')
+#     # print('posts dct is', posts)
+#     # print('template is', template)
+#     context = {'posts': posts}
+#     # print('context is', context)
+#     # body = template.render(context)
+#     # print('body is', body)
+#     # return HttpResponse(body, content_type='text/html')
+#     return render(request, 'blogging/list.html', context)
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    print('request is', request)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    print('context is', context)
-    print('request is', request)
-    return render(request, 'blogging/detail.html', context)
-    
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     print('request is', request)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     print('context is', context)
+#     print('request is', request)
+#     return render(request, 'blogging/detail.html', context)
+
+# Class-based views
+class PostListView(ListView):
+    model = Post
+    queryset = Post.objects.order_by('-published_date').exclude(published_date__exact=None)
+    template_name = 'blogging/list.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    queryset = Post.objects.exclude(published_date__exact=None)
+    print(queryset)
+    template_name = 'blogging/detail.html'
--- polling/views.py
+++ polling/views.py
@@ -1,31 +1,61 @@
 from django.shortcuts import render
 from django.http import Http404
 from polling.models import Poll
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
-def list_view(request):
-    print('list_view request is:', request)
-    print('list view poll obj is:', Poll.objects.all())
-    print('poll obj type is:', type(Poll.objects.all()))
-    print('poll obj type len is:', len(Poll.objects.all()))
-    context = {'polls': Poll.objects.all()}
-    print('list_view context is:', context)
-    return render(request, 'polling/list.html', context)
+# Create your function-based views here.
+# def list_view(request):
+#     # print('list_view request is:', request)
+#     # print('list view poll obj is:', Poll.objects.all())
+#     # print('poll obj type is:', type(Poll.objects.all()))
+#     # print('poll obj type len is:', len(Poll.objects.all()))
+#     context = {'polls': Poll.objects.all()}
+#     # print('list_view context is:', context)
+#     return render(request, 'polling/list.html', context)
 
-def detail_view(request, poll_id):
-    print('detail view request:', request)
-    print('detail view poll_id:', poll_id)
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExit:
-        raise Http404
 
-    if request.method == "POST":
+# def detail_view(request, poll_id):
+#     # print('detail view request:', request)
+#     # print('detail view poll_id:', poll_id)
+#     try:
+#         poll = Poll.objects.get(pk=poll_id)
+#     except Poll.DoesNotExit:
+#         raise Http404
+
+#     if request.method == "POST":
+#         if request.POST.get("vote") == "Yes":
+#             poll.score += 1
+#         else:
+#             poll.score -= 1
+#         poll.save()
+
+#     context = {'poll': poll}
+#     return render(request, 'polling/detail.html', context)
+
+# Create class-based views here
+class PollListView(ListView):
+    model = Poll
+    template_name = 'polling/list.html'
+
+class PollDetailView(DetailView):
+    model = Poll
+    template_name = 'polling/detail.html'
+    
+    def post(self, request, *args, **kwargs):
+        poll = self.get_object()
+        
         if request.POST.get("vote") == "Yes":
             poll.score += 1
         else:
             poll.score -= 1
         poll.save()
+        
+        context = {'object': poll}
+        return render(request, 'polling/detail.html', context)
+        
+        
+    
+    
 
-    context = {'poll': poll}
-    return render(request, 'polling/detail.html', context)
+
