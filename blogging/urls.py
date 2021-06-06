5d6ca98058c3a5e725aaed7ae3ee7d60fea92680
mcmd08 <mmdacutanan@gmail.com>
2021-06-06 01:49:26 -0700

    updated urls.py

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path, include
-from blogging.views import stub_view, PostListView, PostDetailView
+from blogging.views import stub_view, PostListView, PostDetailView, UserViewSet, PostViewSet, CategoryViewSet
 from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'post', PostViewSet)
+router.register(r'category', CategoryViewSet)
 
 urlpatterns = [  # path('', stub_view, name = 'blog_index'),
     path("", PostListView.as_view(), name="blog_index"),
     path("posts/<int:pk>", PostDetailView.as_view(), name="blog_detail"),
-    path("api/", include(routers.urls)),
+    path("api/", include(router.urls)),
     path("api-auth/", include('rest_framework.urls', namespace= 'rest_framework')),
 ]
